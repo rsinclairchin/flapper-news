e5db02fca7eeb861f3f46fd7a787c255c213039d
r.sinclairchin <r.sinclairchin@gmail.com>
2016-03-02 15:05:31 -0800

    login/logout, user authorization

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -28,5 +28,27 @@ function($stateProvider, $urlRouterProvider) {
       }
     })
 
+    .state('login', {
+      url: '/login',
+      templateUrl: 'auth/_login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth){
+        Auth.currentUser().then(function(){
+          $state.go('home')
+        })
+      }]
+    })
+
+    .state('register', {
+      url: '/register',
+      templateUrl: 'auth/_register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth){
+        Auth.currentUser().then(function(){
+          $state.go('home')
+        })
+      }]
+    })
+
     $urlRouterProvider.otherwise('home');
 }])
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 //= require angular
 //= require angular-ui-router
 //= require angular-rails-templates
-//= require angular-devise
+//= require AngularDevise
 //= require_tree .
